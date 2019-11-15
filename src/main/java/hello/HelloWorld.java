06061f107e485eddc57b01720f8526cbb6e4f04b
sandhymo <57794729+sandhymo@users.noreply.github.com>
2019-11-15 16:32:00 +0530

    Add files via upload

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SandhyaMohanty
+ */
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
